763a8aaa5699bafdbc38cc1c86f3908ed6261859
Bhumika-Roy <bhumikaroykgp@gmail.com>
2025-07-22 10:37:39 +0530

    Add files via upload

---
+++ DAY_02/calculator_pro_call.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Program
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Calculations C = new Calculations();
+            Console.WriteLine("Welcome to the calculator app");
+            C.Display(); // Calls a method from Calculations class
+        }
+    }
+}
